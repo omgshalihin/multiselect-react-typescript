fa0ac65aadfc7cd409ed981fdb5daa3f7c499615
omgshalihin <shalihinsgp@gmail.com>
2023-01-13 15:12:29 +0100

    Single select works flawlessly

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Select from "./Select";
+
+const options = [
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Third", value: 3 },
+  { label: "Fourth", value: 4 },
+  { label: "Fifth", value: 5 },
+];
 
 const App = () => {
-  return <div>Hi</div>;
+  const [value, setValue] = useState<typeof options[0] | undefined>(options[0]);
+  return (
+    <div>
+      <Select
+        options={options}
+        value={value}
+        onChange={(opt) => setValue(opt)}
+      />
+    </div>
+  );
 };
 
 export default App;
